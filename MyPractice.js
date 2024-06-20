a0f2b1c9d117ca0e259a49378a7bee747582cbde
Abilash-Nambi <142148604+Abilash-Nambi@users.noreply.github.com>
2024-06-20 15:11:09 +0530

    started array practice

--- MyPractice.js
+++ MyPractice.js
@@ -234,3 +234,32 @@
 // Promise.race([p1, p2, p3]).then((res) => console.log(res));
 // Promise.allSettled([p1, p2, p3]).then((res) => console.log(res));
 // Promise.any([p1, p2, p3]).then((res) => console.log(res));
+
+// function PV(rate, periods, payment, future, type) {
+//   // Initialize type
+//   var type = typeof type === "undefined" ? 0 : type;
+
+//   // Evaluate rate and periods (TODO: replace with secure expression evaluator)
+//   rate = eval(rate);
+//   periods = eval(periods);
+
+//   // Return present value
+//   if (rate === 0) {
+//     return -payment * periods - future;
+//   } else {
+//     return (
+//       (((1 - Math.pow(1 + rate, periods)) / rate) *
+//         payment *
+//         (1 + rate * type) -
+//         future) /
+//       Math.pow(1 + rate, periods)
+//     );
+//   }
+// }
+// console.log(PV(0.05, 120, 700515.64, 0, 1));
+
+function PV(rate, nper, pmt) {
+  return (pmt / rate) * (1 - Math.pow(1 + rate, -nper));
+}
+
+console.log(PV(0.05, 120, 1510000));
